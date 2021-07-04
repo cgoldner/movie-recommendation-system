15904c3870c9e8c9490264bf185350d184510d22
cgoldner <cgoldner@gmx.de>
2021-07-04 11:58:34 +0200

    add wrangle-data

---
+++ wrangle-data.R
@@ -0,0 +1,68 @@
+##########################################################
+# Create edx set, validation set (final hold-out test set)
+##########################################################
+
+# Note: this process could take a couple of minutes
+
+if(!require(tidyverse)) install.packages("tidyverse", repos = "http://cran.us.r-project.org")
+if(!require(caret)) install.packages("caret", repos = "http://cran.us.r-project.org")
+if(!require(data.table)) install.packages("data.table", repos = "http://cran.us.r-project.org")
+
+library(tidyverse)
+library(caret)
+library(data.table)
+library(stringr)
+
+# MovieLens 10M dataset:
+# https://grouplens.org/datasets/movielens/10m/
+# http://files.grouplens.org/datasets/movielens/ml-10m.zip
+
+dl <- tempfile()
+download.file("http://files.grouplens.org/datasets/movielens/ml-10m.zip", dl)
+
+ratings <- fread(text = gsub("::", "\t", readLines(unzip(dl, "ml-10M100K/ratings.dat"))),
+                 col.names = c("userId", "movieId", "rating", "timestamp"))
+
+movies <- str_split_fixed(readLines(unzip(dl, "ml-10M100K/movies.dat")), "\\::", 3)
+colnames(movies) <- c("movieId", "title", "genres")
+
+
+# if using R 4.0 or later:
+movies <- as.data.frame(movies) %>% mutate(movieId = as.numeric(movieId),
+                                           title = as.character(title),
+                                           genres = as.character(genres))
+
+
+movielens <- left_join(ratings, movies, by = "movieId")
+
+# Validation set will be 10% of MovieLens data
+set.seed(1, sample.kind="Rounding")
+test_index <- createDataPartition(y = movielens$rating, times = 1, p = 0.1, list = FALSE)
+edx <- movielens[-test_index,]
+temp <- movielens[test_index,]
+
+# Make sure userId and movieId in validation set are also in edx set
+validation <- temp %>% 
+  semi_join(edx, by = "movieId") %>%
+  semi_join(edx, by = "userId")
+
+# Add rows removed from validation set back into edx set
+removed <- anti_join(temp, validation)
+edx <- rbind(edx, removed)
+
+rm(dl, ratings, movies, test_index, temp, movielens, removed)
+
+# Extract year of release from title column for edx and validation
+validation <- validation %>%
+  mutate(year = str_extract(title, " \\(\\d*\\)$")) %>%
+  mutate(year = str_remove(year, "\\("), year = str_remove(year, "\\)")) %>%
+  mutate(year = as.integer(year)) %>%
+  mutate(title = str_remove(title, " \\(\\d*\\)$"))
+edx <- edx %>%
+  mutate(year = str_extract(title, " \\(\\d*\\)$")) %>%
+  mutate(year = str_remove(year, "\\("), year = str_remove(year, "\\)")) %>%
+  mutate(year = as.integer(year)) %>%
+  mutate(title = str_remove(title, " \\(\\d*\\)$"))
+
+save(edx, file = "rda/edx.rda")
+save(validation, file = "rda/validation.rda")
